2566f2eec144b002bf8a041d0a8a45c80de86ace
Aaron Buxbaum <me@aaronbuxbaum.com>
2019-06-18 00:49:46 -0400

    solaeized

--- sync-terminal.sh
+++ sync-terminal.sh
@@ -1,6 +1,6 @@
 # Copy everything into the filesystem
 yes | cp -R files/. ~/
-yes | cp -R color-scheme/solarized/vim-colors-solarized/colors/. ~/.vim/colors/
+yes | cp -R solarized/vim-colors-solarized/colors/. ~/.vim/colors/
 
 # Copy local environment variables
 test -e .env && yes | cp .env ~/.env.local
